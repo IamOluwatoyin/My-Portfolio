950e9d7399364388fb77850ce12614e71769c397
Oluwatoyin Solomon <toyinsolomon4@gmail.com>
2026-03-19 08:39:46 +0100

    change the resume

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -398,7 +398,7 @@ const Body = () => {
           font-body
           font-semibold
         "
-              href="https://drive.google.com/file/d/1VGGYvVz9_MsfoMdCtakruCZ3gg32Co8R/view?usp=sharing"
+              href="https://drive.google.com/file/d/12_gwxKuV8S4jLG185nEjElOTY9ZKxLpz/view?usp=sharing"
               rel="noopener noreferrer"
             >
               View Resume
